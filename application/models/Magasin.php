e3ac191a8c838caca8216d6174af7818f2e16426
jsteve27 <jsteve.villano@gmail.com>
2014-02-19 17:50:26 -0500

    s

--- application/models/Magasin.php
+++ application/models/Magasin.php
@@ -27,5 +27,25 @@ class App_Model_Magasin extends App_Db_Table_Abstract {
                 var_dump($e->getMessage());			
         }
     }
+    
+    public function magasinInfo($idMagasin){
+        
+       $query = "SELECT a.id_me as id_me , a.nom as nom, a.gerant as gerant ,a.adresse as adresse, a.email as email , a.societe as societe , a.ouverture as ouverture,
+        a.ad_ville as ad_ville , a.ville as ville , a.horaires as horaires , a.tel as tel , a.fax as fax , a.dep as dep,
+        a.latitude as latitude, a.longitude as longitude, a.liengmap as liengmap, a.espace_scoot as espace_scoot,
+        a.acces as acces , a.actif as actif , b.responsable as responsablezone, b.email as emailzone, c.nom_fichier as fichier_photo
+        FROM moto_expert a 
+        LEFT JOIN zones b 
+        ON a.id_zone=b.id_zone
+        LEFT JOIN magasins_photos c
+        ON	a.id_me=c.id_magasin
+        WHERE a.id_me='".$idMagasin."'";   
+       try{
+        return $this->getAdapter()->fetchPairs($query);
+         
+       }catch (Exception $e){
+                var_dump($e->getMessage());
+       }
+    }
 
 }
