514d229589fd6e7deac27ac457dfc1f596681382
James Otiniano <jotiniano@multibox.pe>
2014-02-20 21:00:09 -0500

    historico

---
+++ application/models/ClientConnexion.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Description of User
+ *
+ * @author James Otiniano
+ */
+class App_Model_ClientConnexion extends App_Db_Table_Abstract {
+
+	protected $_name = 'client_connexion';
+
+	const TABLE_CLIENTCON = 'client_connexion';
+	
+	public function updateData($datos, $id) {
+		$id = $this->update($datos, 'id_client = ' . $id);
+		return $id; 
+	}
+	
+	public function insertData($datos) {
+		
+		$id = $this->insert($datos);
+		return $id;
+	
+	}
+	
+	
+	
+
+	
+}
