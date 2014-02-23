95bab868994353103bdcc96b92ba08f55812ca95
James Otiniano <jotiniano@multibox.pe>
2014-02-23 11:10:39 -0500

    mi cuenta

---
+++ application/modules/compte/controllers/ModifierClientController.php
@@ -0,0 +1,25 @@
+<?php
+
+class Compte_ModifierClientController extends App_Controller_Action_Default
+{
+
+    public function init()
+    {
+        parent::init();
+        
+    }
+    
+    public function indexAction()
+    {
+    	$modelanunce = new App_Model_Announce();    	
+    	$this->view->ruta = $this->config->app->photoUrl;    	
+    	$this->view->result = 
+    	$modelanunce->announceByCLient($this->view->authData->cid);
+    	
+    	
+        
+    }
+    
+    
+}
+
