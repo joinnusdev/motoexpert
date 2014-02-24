c548a396e0cde99d1f00668e0393b5121c464817
jsteve27 <jsteve.villano@gmail.com>
2014-02-24 11:28:58 -0500

    s

--- library/App/Controller/Action/Default.php
+++ library/App/Controller/Action/Default.php
@@ -4,9 +4,12 @@ class App_Controller_Action_Default extends App_Controller_Action
 	public function init()
 	{
 		parent::init();
+                
                 $formTienda = new App_Form_BuscarTienda();
-        $this->view->formMagasin = $formTienda;
+                $this->view->formMagasin = $formTienda;
 		$this->_helper->layout->setLayout('layout');
+                
+                
 
 	}
 }
