f9c139c851fb4b7ba88fb8f9a16a3286715bafb2
jsteve27 <jsteve.villano@gmail.com>
2014-02-25 00:10:02 -0500

    s

---
+++ application/modules/default/controllers/MentionsController.php
@@ -0,0 +1,18 @@
+<?php
+
+class Default_MentionsController extends App_Controller_Action_Default
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+    
+    public function indexAction()
+    {
+       
+
+    } 
+
+}
+
