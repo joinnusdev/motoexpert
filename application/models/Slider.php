6a9fc6d1ccfbc9d2b23e33b132abaf6f298e9147
jsteve27 <jsteve.villano@gmail.com>
2014-03-04 23:54:38 -0500

    s

--- application/models/Slider.php
+++ application/models/Slider.php
@@ -4,8 +4,13 @@ class App_Model_Slider extends App_Db_Table_Abstract {
     protected $_name = 'hicone-mobile-datas-bannieres';
 
     public function listSlider(){
-       /* $query = $this->_db
-        ->select()->from(array('s' => $this->_name), array('id', 'value'));
-        return $this->_db->fetchAll($query);*/
+        $query = "select * from `hicone-mobile-datas-bannieres`
+                    where  right(id,3) = 'img'";
+            try{
+                return $this->getAdapter()->fetchAll($query);
+
+            }catch (Exception $e){
+                        var_dump($e->getMessage());
+            }
     }
 }
