e179d32c5f548e9fc4b0901d1c2d064bfa4d875d
James Otiniano <jotiniano@multibox.pe>
2014-03-06 18:56:38 -0500

    modifier annonce

--- application/modules/default/controllers/NewsController.php
+++ application/modules/default/controllers/NewsController.php
@@ -36,12 +36,28 @@ class Default_NewsController extends App_Controller_Action_Default
         		}
         	}
         }
-         
-       
     } 
     
     public function detailAction()
     {
+    	$modelNoticia = new App_Model_Noticia();    	
+    	$datos = $this->getAllParams();    	
+    	$page = $this->_getParam('cod_new', NULL);
+    	
+    	if ($this->_request->isGet()) {
+    		if ($datos and !is_null($datos)) {    	
+    			$result = $modelNoticia->listNews();
+    			 
+    			$paginator = Zend_Paginator::factory($result);
+    			$paginator->setCurrentPageNumber($page)
+    			->setItemCountPerPage(1);
+    			$this->view->result = $paginator;
+    			 
+    			if ($this->view->result){
+    				$this->view->total = count($result);
+    			}
+    		}
+    	}
     	
     }
     
