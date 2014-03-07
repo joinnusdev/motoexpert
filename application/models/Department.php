93537c7796af159ab527921253ccd30bac418f2e
jsteve27 <jsteve.villano@gmail.com>
2014-03-06 21:28:15 -0500

    s

--- application/models/Department.php
+++ application/models/Department.php
@@ -5,7 +5,9 @@ class App_Model_Department extends App_Db_Table_Abstract {
 
     public function listDepartment(){
         $query = $this->_db
-        ->select()->from(array('d' => $this->_name), array("numero AS NUM", "CONCAT(numero, ' - ',nom) AS NOM"));
+        ->select()->from(array('d' => $this->_name), array("numero AS NUM", "CONCAT(numero, ' - ',nom) AS NOM"))
+        ->where('d.pays = ?', 'fr');
+        	
         
         return $this->_db->fetchPairs($query);
     }
